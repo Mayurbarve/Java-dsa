97d61a880d2560ae2124784df2e95b49be6196a6
mayurbarve04@gmail.com <136147003+Mayurbarve@users.noreply.github.com>
2026-08-18 21:22:36 +0530

    Rotated Binary baby

---
+++ src/DSAQuestions/BinarySearch/RotatedBinarySearch.java
@@ -0,0 +1,37 @@
+    void main() {
+        int[] nums = {4,5,6,7,0,1,2};
+        int target = 0;
+
+        System.out.println(rotatedSearch(nums, target));
+    }
+
+    public int rotatedSearch(int[] nums, int target) {
+
+        int left = 0;
+        int right = nums.length - 1;
+
+        while (left <= right) {
+            int mid = left + (right - left) / 2;
+            if (nums[mid] == target) return mid;
+
+            if(nums[left] <= nums[mid]){
+                if(nums[left] <= target && target < nums[mid]){
+                    right = mid - 1;
+                }
+                else{
+                    left = mid + 1;
+                }
+            }
+            else{
+                if(nums[right] <= target && target < nums[mid]){
+                    left = mid + 1;
+                }
+                else{
+                    right = mid - 1;
+                }
+            }
+
+        }
+
+        return -1;
+    }
