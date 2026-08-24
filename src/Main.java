c6583705f3c3c50397f78cd56acc600bf1fbfef2
mayurbarve04@gmail.com <136147003+Mayurbarve@users.noreply.github.com>
2026-08-24 21:13:16 +0530

    D Minor

--- src/Main.java
+++ src/Main.java
@@ -2,4 +2,26 @@
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 void main() {
     System.out.println("This Repository belong to My Java Learning Phase ");
+
+    int[] nums = {1,3, 3, 4,1, 1,};
+
+    System.out.println(numIdenticalPairs(nums));
+
 }
+
+    public int numIdenticalPairs(int[] nums) {
+
+        int[] freq = new int[101];
+        int count = 0;
+
+        for (int num : nums) {
+            if (freq[num] > 0) {
+                count += freq[num];
+                freq[num]++;
+
+            } else {
+                freq[num] = 1;
+            }
+        }
+        return count;
+    }
